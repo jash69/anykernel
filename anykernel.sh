116be38b5f9b36b7f5a63ea7bbe50b0cd07176db
Jash <jashvakharia@gmail.com>
2022-08-24 11:27:31 +0530

    update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Sleepy Kernel by shashank1439
+kernel.string=OOF_sweet kernel by brucetech
 do.devicecheck=1
 do.modules=0
 do.systemless=1
